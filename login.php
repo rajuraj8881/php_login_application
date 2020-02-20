1e66a7596f384890a29cb4837acf038c5432481b
Raju Mondal <45078493+rajuraj8881@users.noreply.github.com>
2020-02-21 04:01:59 +0600

    initial commit

---
+++ login.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Login</h2>
+    </div>
+    <form method="post" action="login.php">
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="login" class="btn">Login</button>
+        </div>
+        <p>
+            Not yet a member? <a href="register.php">Sign up</a>
+        </p>
+    </form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,43 @@
+<?php
+    include('server.php');
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Register</h2>
+    </div>
+    <form method="post" action="register.php">
+        <div class="input-group">
+            <label>Username</label>
+            <input type="text" name="username">
+        </div>
+        <div class="input-group">
+            <label>Email</label>
+            <input type="text" name="email">
+        </div>
+        <div class="input-group">
+            <label>Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div class="input-group">
+            <label>Confirm Password</label>
+            <input type="password" name="password_2">
+        </div>
+        <div class="input-group">
+            <button type="submit" name="register" class="btn">Register</button>
+        </div>
+        <p>
+            Already a member? <a href="login.php">Sign in</a>
+        </p>
+    </form>
+</body>
+</html>
